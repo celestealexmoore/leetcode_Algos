be95b36fc26293d921b0cf0ae9d64cf566018d70
cMoore <celestealexmoore@gmail.com>
2022-08-04 11:40:31 -0500

    refactored

--- easy-algos/fizzbuzz/fizzbuzz.js
+++ easy-algos/fizzbuzz/fizzbuzz.js
@@ -19,20 +19,6 @@ FizzBuzz
 ...
 */
 
-/* 
-My Pseudocode:
-1. Output all numbers 0 >= int:
-> Create an array with the int as an index. 
-> Find a js array method that inserts new index into an array at position 0.
-> Write a function that will take the number at index 0 of the array, subtract 1, and update the array with the new int, until the number is === 1.
-
-Find a js array method that will replace indexes in an array with a new value.
-Loop over the array and determine:
-2. Is the array[i] % 3 === 0 ? replace num with "Fizz".
-2. Is the array[i] % 5 === 0 ? replace num with "Buzz".
-3. Is the array[i] % 3 === 0 && number % 5 === 0 ? replace num with "FizzBuzz".
-*/
-
 function fizzbuzz(num) {
   for(i = 1; i <= num; i++){
     if (i % 3 === 0 && i % 5 === 0) {
